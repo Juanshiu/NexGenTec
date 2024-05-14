ac690f8b5ca00deedb558d1521a17230843a83b8
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-05-14 11:05:32 -0500

    Añadiendo componente para formatear url de los productos

---
+++ src/components/FormaterUrl.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let url: string;
+    export let className = '';
+
+    function crearSlug(nombre: string) {
+        return nombre.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
+    }
+</script>
+
+<a href={crearSlug(url)} class={`flex text-center font-semibold bg-black hover:scale-105 transition-transform text-white px-4 py-2 rounded text-md ${className}`}>
+    <slot />
+</a>
