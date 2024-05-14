492fb656acbc9cd6716a2275ee43889172085583
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-05-14 11:57:30 -0500

    Actualizar importación de supabase en Searching.svelte

--- src/components/layout/Searching.svelte
+++ src/components/layout/Searching.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import type Products from "../../interface/productos";
-import { supabase } from "@lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 let searchTerm = "";
 let searchResults: Products[] = [];
