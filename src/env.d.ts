4723d0001cf626ef365a788e60b4ff308f5a016f
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-05-14 11:17:50 -0500

    Remover importaciones innecesarias

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,3 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-
 interface ImportMetaEnv {
     readonly SUPABASE_URL: string
     readonly SUPABASE_ANON_KEY: string
