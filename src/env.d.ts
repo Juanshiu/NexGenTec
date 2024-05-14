2adec498d1b29321aaf3bce5824313ff9264e68e
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-05-14 11:35:49 -0500

    Actualizar URL de redirección de OAuth para el punto de conexión de inicio de sesión API

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,14 @@
+
 interface ImportMetaEnv {
-    readonly SUPABASE_URL: string
-    readonly SUPABASE_ANON_KEY: string
+  readonly SUPABASE_URL: string
+  readonly SUPABASE_ANON_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+declare namespace App {
+  interface Locals {
+    email: string;
   }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
-  declare namespace App {
-    interface Locals {
-      email: string;
-    }
-  }
+}
